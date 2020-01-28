142450d273eca70ed93625a08c62dc51f178033a
que master <quoeamaster@gmail.com>
2020-01-28 18:55:27 +0800

    20200128d

--- es_search_demo_webapp/component/search_header.js
+++ es_search_demo_webapp/component/search_header.js
@@ -22,7 +22,10 @@ Vue.component('search-header', {
       //this.$emit('onQueryRequest', { 'query': q });
     },
     showAbout: function () {
-        
+      document.querySelector('#h-modal-dlg').style.display = 'block';
+    },
+    closeModal: function () {
+      document.querySelector('#h-modal-dlg').style.display = 'none';
     }
 
   },
@@ -41,6 +44,53 @@ Vue.component('search-header', {
     <span class="h-caption cursor-pointer" v-on:click="showAbout()">
       About
     </span>
+  </div>
+  
+  <!-- modal -->
+  <!-- The Modal -->
+  <div id="h-modal-dlg" class="h-modal">
+    <span class="h-modal-close" v-on:click="closeModal()">&times;</span>
+    <div class="h-modal-container">
+      <div style="text-align: center;">
+        <span style="font-family: 'Comic Sans MS'; font-size: 1.2em; color: #666;">bookstore Eureka</span> 
+        is developed By 
+        <span style="color: #FF9800; font-family: 'Comic Sans MS'; font-size: 1.1em;">Jason Wong</span>
+      </div>
+      <div style="width: 350px; margin: auto; margin-bottom: 40px;">
+        <span style="font-size: 1.1em;">technology stack</span>
+        <ul style="line-height: 40px;">
+          <li class="h-modal-caption">
+            <span class="h-modal-li-caption">datastore</span> 
+            <span class="h-modal-li-bold-caption float-right">elasticsearch 7.5.x</span></li>
+          <li class="h-modal-caption">
+            <span class="h-modal-li-caption">frontend</span> 
+            <span class="h-modal-li-bold-caption float-right">vue.js 2.6.x</span></li>
+          <li class="h-modal-caption">
+            <span class="h-modal-li-caption">css</span> 
+            <span class="h-modal-li-bold-caption float-right">mdbootstrap 4.8.11</span></li>
+          <li class="h-modal-caption">
+            <span class="h-modal-li-caption">libraries</span> 
+            <span class="h-modal-li-bold-caption float-right">jQuery 3.4.1</span> 
+            <span class="h-modal-li-bold-caption float-right">vuex 3.1.x,&nbsp;</span></li>
+          <li class="h-modal-caption">
+            <span class="h-modal-li-caption">font resources</span> 
+            <span class="h-modal-li-bold-caption float-right">font-awesome 5.8.2</span></li>
+        </ul>
+      </div>
+      <!-- social media connections -->
+      <div style="text-align: center;">
+        <i class="fab fa-github cursor-pointer" 
+          style="color: #666; font-size: 1.5em; margin-left: 4px; margin-right: 4px;" 
+          onclick="window.open('https://github.com/quoeamaster', '_social');"></i>
+        <i class="fab fa-medium cursor-pointer" 
+          style="color: #666; font-size: 1.5em; margin-left: 4px; margin-right: 4px;"
+          onclick="window.open('https://medium.com/@quoeamaster', '_social');"></i>
+        <i class="fab fa-wordpress-simple cursor-pointer" 
+          style="color: #666; font-size: 1.5em; margin-left: 4px; margin-right: 4px;"
+          onclick="window.open('https://bigdata887.wordpress.com/2019/11/23/golang-dependency-management-part-1/', '_social');"></i>
+      </div>
+      <div class="float-right" style="display: block;">version <span style="font-family: 'Comic Sans MS'; font-size: 0.9em;">1.0.0</span></div>
+    </div>
   </div>
   
 </div>
